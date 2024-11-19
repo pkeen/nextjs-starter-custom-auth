891f55038957ce511f5c2fa121920874aa97a8b7
pkeen <pkeen7@gmail.com>
2024-11-19 12:02:53 -0800

    add password hashing

--- src/lib/auth/login/authorize.ts
+++ src/lib/auth/login/authorize.ts
@@ -1,35 +1,39 @@
 "use server";
+import { hashPassword, verifyPassword } from "@/utils/password";
 
-const testUser = {
+let testUser = {
+	id: "1",
+	name: "Peter Keen",
 	email: "pkeen7@gmail.com",
 	password: "12345678",
 };
 
 interface credentials {
-    email: string;
-    password: string;
-};
+	email: string;
+	password: string;
+}
 
 export const authorize = async (credentials: credentials) => {
-    console.log("Authorize called with:", credentials?.email);
-
+	console.log("Authorize called with:", credentials?.email);
 	const { email, password } = credentials || {};
 
-	console.log("password: ", password);
+	// first hash the test password
+	testUser.password = await hashPassword(testUser.password);
 
-	if (email === testUser.email) {
-		console.log("email matches");
+	try {
+		// verify user exist with given email
+		const user = email === testUser.email ? testUser : null;
+		if (!user) {
+			throw new Error("No user found");
+		}
+		const isValid = await verifyPassword(password as string, user.password);
+		if (!isValid) {
+			throw new Error("Invalid password");
+		}
+		console.log("User logged in successfully");
+		return user;
+	} catch (error) {
+		console.error("Error authorizing user, unkown error:", error);
+		return null;
 	}
-
-	if (password === testUser.password) {
-		console.log("password matches");
-	}
-
-	const user =
-		email === testUser.email && password === testUser.password
-			? testUser
-			: null;
-
-	return user;
-
-}
+};
